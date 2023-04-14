8ad3e324e6df238f88436fa80a01f6539a31d09b
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-14 18:55:56 -0500

    updated css

--- src/Weather.js
+++ src/Weather.js
@@ -3,19 +3,40 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            ></input>
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary"
+            ></input>
+          </div>
+        </div>
+      </form>
       <h1>Harare</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          >
-            8℃
-          </img>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <span className="temperature">8</span>
+            <span className="unit">℃</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
