ee12507a08cb96d022472fbb529bb9198f048047
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-14 12:29:24 -0500

    new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Josephine Marecha and is {""}
-        <a href="https://github.com/dahnedewy/my-app" target="blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Josephine Marecha and is {""}
+          <a href="https://github.com/dahnedewy/my-app" target="blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
