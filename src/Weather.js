f8d3fcfdd5a4777731625a126f8e98f84ff27c84
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-15 00:35:06 -0500

    update js

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -69,8 +69,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
-    let city = "Harare";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading..";
