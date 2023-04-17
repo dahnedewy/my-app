2954c595f115bfec003ef038a6e87dc9e6e24563
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-17 13:50:17 -0500

    updated components

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       wind: 12,
       city: response.data.name,
     });
